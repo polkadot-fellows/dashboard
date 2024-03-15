3431d43d87a16090811c451cd0ee36db6ac1af14
Nikos Kontakis <wirednkod@gmail.com>
2024-03-15 20:00:27 +0200

    more changes

--- src/library/SideMenu/index.tsx
+++ src/library/SideMenu/index.tsx
@@ -3,13 +3,14 @@
 import { useState } from "react"
 
 import { PolkadotUrl } from "consts"
-import { Menu, Switch, Layout, Button } from "antd"
+import { Menu, Layout, Button, Divider } from "antd"
+import type { GetProp, MenuProps } from "antd"
+
 import { IconWrapper, LogoWrapper } from "library/SideMenu/Wrapper"
 
 import PolkadotIcon from "img/polkadotIcon.svg?react"
 // import FellowshipW from "img/fellowshipLogo_w.svg?react"
 import FellowshipB from "img/fellowshipLogo_b.svg?react"
-import type { GetProp, MenuProps } from "antd"
 
 import {
   HiMiniUserGroup,
@@ -17,10 +18,21 @@ import {
   HiMiniUserPlus,
   HiBuildingLibrary,
   HiMiniCubeTransparent,
+  HiMiniInboxStack,
 } from "react-icons/hi2"
+
+import {
+  IoSunnyOutline,
+  IoMoon,
+  IoLogoGithub,
+  IoChatbubblesOutline,
+} from "react-icons/io5"
 import { MdDocumentScanner } from "react-icons/md"
 import { GoSidebarCollapse, GoSidebarExpand } from "react-icons/go"
 import { useTheme } from "contexts/Themes"
+import { SiElement } from "react-icons/si"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons"
 
 const { Sider } = Layout
 
@@ -43,12 +55,19 @@ const getItem = (
 const getLink = (
   label: string,
   link: string = "#",
+  isLocal: boolean = true,
   target: "_parent" | "_blank" = "_parent"
 ): React.ReactNode => (
-  <a href={"#/" + link} target={target} rel="noopener noreferrer">
+  <a
+    href={isLocal ? "#/" + link : link}
+    target={target}
+    rel="noopener noreferrer"
+  >
     {label}
   </a>
 )
+
+const iconSize = "1.25rem"
 
 const mainItems: MenuItem[] = [
   getItem(getLink("Overview", "overview"), "1", <HiGlobeAlt />),
@@ -68,21 +87,59 @@ const mainItems: MenuItem[] = [
     "6",
     <HiMiniCubeTransparent />
   ),
-  getItem(getLink("RFCs", "rfcs"), "8", <MdDocumentScanner />),
+  getItem(getLink("Modules", "modules"), "8", <HiMiniInboxStack />),
+  getItem(getLink("Open RFCs", "rfcs"), "9", <MdDocumentScanner />),
+]
+
+const secondaryItems: MenuItem[] = [
+  getItem("Fellowship Element", "sub1", <SiElement />, [
+    getItem(
+      getLink(
+        "Members",
+        "https://matrix.to/#/#fellowship-members:parity.io",
+        false,
+        "_blank"
+      ),
+      "sub1-1",
+      <SiElement />
+    ),
+    getItem(
+      getLink(
+        "Open",
+        "https://matrix.to/#/#fellowship-open-channel:parity.io",
+        false,
+        "_blank"
+      ),
+      "sub1-1",
+      <IoChatbubblesOutline />
+    ),
+  ]),
+  // getItem(getLink("Members", "members"), "2", <HiMiniUserGroup />),
+  // getItem(getLink("Membership", "membership"), "3", <HiMiniUserPlus />),
+  // //   [
+  // //     getItem("Option 3", "3"),
+  // //     getItem("Option 4", "4"),
+  // //     getItem("Submenu", "sub1-2", null, [
+  // //       getItem("Option 5", "5"),
+  // //       getItem("Option 6", "6"),
+  // //     ]),
+  // //   ]),
+  // getItem(getLink("Governance", "governance"), "4", <HiBuildingLibrary />),
+  // getItem(
+  //   getLink("Interactions", "interactions"),
+  //   "6",
+  //   <HiMiniCubeTransparent />
+  // ),
+  // getItem(getLink("RFCs", "rfcs"), "8", <MdDocumentScanner />),
 ]
 
 export const SideMenu = () => {
   const [collapsed, setCollapsed] = useState(false)
-  const [type, setType] = useState<"vertical" | "inline">("inline")
+  const [type, setType] = useState<"vertical" | "inline">("vertical")
   const { mode, toggleTheme } = useTheme()
 
-  console.log("mode", mode)
-
-  const changeType = (value: boolean) => {
-    setType(value ? "vertical" : "inline")
-  }
-
-  const size = collapsed ? "1.4rem" : "2.2rem"
+  const size = collapsed ? "1.8rem" : "2.2rem"
+  const bottomWidth = collapsed ? "5rem" : "14rem"
   const Svg = collapsed ? (
     <PolkadotIcon
       style={{
@@ -111,46 +168,99 @@ export const SideMenu = () => {
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
+      trigger={null}
     >
-      <LogoWrapper
-        $minimised={collapsed}
-        onClick={() => window.open(PolkadotUrl, "_blank")}
-      >
-        <IconWrapper
+      <section>
+        <LogoWrapper
           $minimised={collapsed}
-          className="icon"
-          style={{ width: size, height: size }}
+          onClick={() => window.open(PolkadotUrl, "_blank")}
         >
-          {Svg}
-        </IconWrapper>
-      </LogoWrapper>
-      <Menu
-        style={{ width: "100%" }}
-        defaultSelectedKeys={["1"]}
-        mode={type}
-        theme={mode}
-        items={mainItems}
-      />
-      <Button
-        onClick={() => setCollapsed(!collapsed)}
+          <IconWrapper
+            $minimised={collapsed}
+            className="icon"
+            style={{ width: size, height: size }}
+          >
+            {Svg}
+          </IconWrapper>
+        </LogoWrapper>
+        <Menu
+          style={{ width: "100%" }}
+          defaultSelectedKeys={["1"]}
+          mode={type}
+          theme={mode}
+          items={mainItems}
+        />
+        <Menu
+          style={{ width: "100%" }}
+          defaultSelectedKeys={["1"]}
+          mode={type}
+          theme={mode}
+          items={secondaryItems}
+        />
+        <Button
+          onClick={() => setCollapsed(!collapsed)}
+          style={{
+            width: "100%",
+            alignContent: "center",
+            border: "0",
+            borderRadius: "0",
+          }}
+        >
+          <div
+            style={
+              collapsed
+                ? { display: "flex", justifyContent: "center" }
+                : { display: "flex", padding: "0 1rem" }
+            }
+          >
+            {collapsed ? <GoSidebarCollapse /> : <GoSidebarExpand />}
+          </div>
+        </Button>
+      </section>
+
+      <section
         style={{
-          width: "100%",
-          alignContent: "center",
-          border: "0",
-          borderRadius: "0",
+          position: "absolute",
+          bottom: "3rem",
+          width: bottomWidth,
+          display: "flex",
+          justifyContent: "space-around",
         }}
       >
-        <div style={{ display: "flex" }}>
-          {collapsed ? <GoSidebarCollapse /> : <GoSidebarExpand />}
-        </div>
-      </Button>
-
-      <div style={{ position: "absolute", bottom: "3rem" }}>
-        <Switch onChange={changeType} />
+        {mode === "dark" ? (
+          <button
+            style={{ color: "white" }}
+            type="button"
+            onClick={() => toggleTheme()}
+          >
+            <IoSunnyOutline size={iconSize} />
+          </button>
+        ) : (
+          <button type="button" onClick={() => toggleTheme()}>
+            <IoMoon size={iconSize} />
+          </button>
+        )}
+        <button
+          style={mode === "dark" ? { color: "white" } : {}}
+          type="button"
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          <FontAwesomeIcon icon={collapsed ? faExpandAlt : faCompressAlt} />
+        </button>
+        <button
+          style={mode === "dark" ? { color: "white" } : {}}
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/polkadot-fellows", "_blank")
+          }
+        >
+          <IoLogoGithub size={iconSize} />
+        </button>
+        {/* <Switch onChange={changeType} />
         <Switch
           onChange={() => toggleTheme(mode === "dark" ? "light" : "dark")}
-        />
-      </div>
+        /> */}
+      </section>
     </Sider>
   )
 }

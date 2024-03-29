edd5b247fb0860f9eec53539f77463372fc7b02e
Nikos Kontakis <wirednkod@gmail.com>
2024-03-29 19:30:38 +0200

    Error page fix

---
+++ src/ErrorPage.tsx
@@ -0,0 +1,19 @@
+export const ErrorPage = () => {
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        Please try to reload the page or{" "}
+        <a
+          href="https://github.com/polkadot-fellows/dashboard/issues"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open an issue
+        </a>
+        .
+      </p>
+    </div>
+  )
+}

cca32971ebf979d1ebfc942fdc1a0fea51214369
Nikos Kontakis <wirednkod@gmail.com>
2024-08-10 04:24:26 +0300

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dashboard',
   plugins: [react(), svgr()],
+  base: '/dashboard/',
   build: {
     outDir: 'build',
   },

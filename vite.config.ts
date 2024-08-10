3b9c6b9976905e3b9a9cdee547b81f6a748100e4
Nikos Kontakis <wirednkod@gmail.com>
2024-08-10 14:15:30 +0300

    Fix deployed first page

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/dashboard/',
+  base: '/dashboard',
   build: {
     outDir: 'build',
   },

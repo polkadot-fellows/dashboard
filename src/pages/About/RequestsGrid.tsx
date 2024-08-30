598119e75fba648f95b0a2f9da8045324b6e523a
peetzweg/ <phil.czek@gmail.com>
2024-08-30 11:39:16 +0200

    remove pagination feature, just annoying.

--- src/pages/About/RequestsGrid.tsx
+++ src/pages/About/RequestsGrid.tsx
@@ -249,7 +249,6 @@ export const RequestsGrid = ({ lcStatus }: LcStatusType) => {
     onSortingChange: setSorting,
     onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
@@ -315,26 +314,6 @@ export const RequestsGrid = ({ lcStatus }: LcStatusType) => {
             </TableBody>
           </Table>
         )}
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
       </div>
       <MemberInfo
         lcStatus={lcStatus}

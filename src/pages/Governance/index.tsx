91efd274c245a312b32ae5e24e0be92fd20d8b13
peetzweg/ <phil.czek@gmail.com>
2024-08-30 15:12:41 +0200

    dry code by setting layout css once for all

--- src/pages/Governance/index.tsx
+++ src/pages/Governance/index.tsx
@@ -12,7 +12,7 @@ import { ThemedLink } from '@/components/ThemedComponents'
 
 export const Governance = () => {
   return (
-    <main className="grid flex-1 items-start gap-4 p-4 sm:mx-[5%] xl:mx-[15%] mx-0 sm:px-6 sm:py-0 md:gap-8">
+    <main>
       <div className="header">
         <h1 className="font-unbounded text-primary flex-1 shrink-0 whitespace-nowrap text-2xl font-semibold tracking-tight sm:grow-0">
           Governance
--- src/pages/Modules/index.tsx
+++ src/pages/Modules/index.tsx
@@ -1,6 +1,6 @@
 export const Modules = () => {
   return (
-    <main className="grid flex-1 items-start gap-4 p-4 sm:mx-[5%] xl:mx-[15%] mx-0 sm:px-6 sm:py-0 md:gap-8">
+    <main>
       <h1 className="font-unbounded text-primary flex-1 shrink-0 whitespace-nowrap text-2xl font-semibold tracking-tight sm:grow-0">
         Modules
       </h1>

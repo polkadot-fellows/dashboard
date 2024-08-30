14c3f91d158eed75f1a90b7c7709d4fd60f30d72
peetzweg/ <phil.czek@gmail.com>
2024-08-30 12:07:23 +0200

    setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
-import { HashRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 
 import './index.css'
+
+const queryClient = new QueryClient()
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
@@ -11,8 +14,10 @@ const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )

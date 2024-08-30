66a9b366db43a3302209cad7ae5fd2863d363c09
peetzweg/ <phil.czek@gmail.com>
2024-08-30 12:47:09 +0200

    make queryclient persist data

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import './index.css'
 
 const queryClient = new QueryClient()
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+})
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
@@ -14,10 +20,16 @@ const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
       <HashRouter>
         <App />
       </HashRouter>
-    </QueryClientProvider>
+      {import.meta.env.MODE === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </PersistQueryClientProvider>
   </StrictMode>,
 )

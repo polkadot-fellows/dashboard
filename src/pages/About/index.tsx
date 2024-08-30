4db1db184be1995b0919111421b3d670d167a3ed
peetzweg/ <phil.czek@gmail.com>
2024-08-30 15:03:44 +0200

    reuse h1 styling

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -12,9 +12,7 @@ export const About = ({ lcStatus }: Props) => {
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:mx-[5%] xl:mx-[15%] mx-0 sm:px-6 sm:py-0 md:gap-8">
       <Content />
-      <h1 className="font-unbounded text-primary flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
-        {`${members?.length || ''} Members`}
-      </h1>
+      <h1>{`${members?.length || ''} Members`}</h1>
       <div className="pageTop">
         List of members and candidates currently inducted in the Fellowship
         Collective.
